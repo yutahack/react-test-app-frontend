630cdf1f8c53eb76c828e7a8bc01455a34dd974b
yutahack <yutahack@gmail.com>
2021-12-23 23:26:33 +0900

    Gql uri local모드 해제

--- src/GraphQL/fetch-graphql.js
+++ src/GraphQL/fetch-graphql.js
@@ -1,8 +1,19 @@
+const dev = require('../config/dev');
+
 async function fetchGraphQL(gqlQuery, params) {
   let v = { loading: false, response: '200', value: '' };
 
+  // GraphQL 접속을 위한 URL 정의
+  let gql_remote = '';
+  if (true === dev.IS_LOCAL_MODE) {
+    gql_remote = 'http://localhost:4001/books';
+  } else {
+    gql_remote = dev.REMOTE_SERVER_ADDRESS + '/books';
+  }
+  console.log('Gql fetch URI= ', gql_remote);
+
   try {
-    const response = await fetch('http://localhost:4001/books', {
+    const response = await fetch(gql_remote, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
---
+++ src/config/dev.js
@@ -0,0 +1,7 @@
+const IS_LOCAL_MODE = false;
+const REMOTE_SERVER_ADDRESS = 'http://193.123.243.82:4000';
+
+module.exports = {
+  IS_LOCAL_MODE,
+  REMOTE_SERVER_ADDRESS,
+};
