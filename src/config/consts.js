e5885b024bc9e3feee83e4d26494a989478d1d0b
yutahack <yutahack@gmail.com>
2022-08-29 21:04:05 +0900

    env 수정

--- src/config/consts.js
+++ src/config/consts.js
@@ -1,5 +1,15 @@
 const REMOTE_SERVER_ADDRESS = process.env.REACT_APP_REMOTE_SERVER_ADDRESS;
 
+switch (process.env.NODE_ENV) {
+  case 'development':
+    console.log('Running in Development Mode');
+    console.log('API Server Endpoint: ', REMOTE_SERVER_ADDRESS);
+    break;
+  case 'production':
+    console.log('Running in Production Mode');
+    break;
+}
+
 module.exports = {
   REMOTE_SERVER_ADDRESS,
 };
